e68807c7c7ba2f5818e5af12b0b20e5c157cd407
gnikhilraj <104124826+gnikhilraj@users.noreply.github.com>
2024-11-25 15:25:39 -0600

    Initial setup

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createTodo } = require('./types');
+const app = express();
+
+app.use(express.json());
+
+
+app.post("/todo",function(req , res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+})
+
+app.get("/todos",function(req , res){
+    
+})
+
+app.put("/completed",function(req , res){
+    
+})
---
+++ ReadMe.md.txt
@@ -0,0 +1,7 @@
+This is a todo Application in which it  
+
+1.Creates TODO
+2.See their existing todo
+3.Mark Todo as done
+4. Delete their existing todos
+130course -- Computer and information systems
