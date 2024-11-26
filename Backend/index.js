7e5caf45c5e6ab691a11997ec8f7d5f730cce470
Nikhil Victus <menikhilraj05@gmail.com>
2024-11-26 13:38:53 -0600

    Done with validation in post and put methods

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createTodo } = require('./types');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
@@ -8,6 +8,13 @@ app.use(express.json());
 app.post("/todo",function(req , res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
+    if(parsedPayload != success){
+        res.status(411).json({
+            msg : "you sent the wrong inputs"
+        })
+        return;
+    }
+    //put it in mongodb
 })
 
 app.get("/todos",function(req , res){
@@ -15,5 +22,12 @@ app.get("/todos",function(req , res){
 })
 
 app.put("/completed",function(req , res){
-    
+    const createPayload = req.body;
+    const parsedPayload = updateTodo.safeParse(createPayload);
+
+    if(parsedPayload != success){
+        res.status(411).json({
+            msg : "you sent the wrong inputss"
+        })
+    }
 })
